63a9040de8479253304aa597baeb45288829823a
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-21 16:48:44 -0500

    Server
    Tracking electron basic Server

---
+++ client/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron/main");
+const path = require("node:path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      focusStyle: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+    icon: path.join(__dirname, "assets/app/logo.png"),
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
