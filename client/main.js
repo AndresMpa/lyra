7e6bbdaecee1c452a2a1537dd488d6791a08882c
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-22 03:59:07 -0500

    Window aspect & shell execution
    Opening links in default OS browser also adding a better aspect ratio
    for client windows

--- client/main.js
+++ client/main.js
@@ -1,18 +1,31 @@
-const { app, BrowserWindow } = require("electron/main");
+const { screen, shell, app, BrowserWindow } = require("electron/main");
 const path = require("node:path");
 
 function createWindow() {
+  const { height } = screen.getPrimaryDisplay().workAreaSize;
+
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: height,
     webPreferences: {
       focusStyle: false,
-      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, "./preloader/preload.js"),
     },
     icon: path.join(__dirname, "assets/app/logo.png"),
   });
 
   win.loadFile("index.html");
+  win.webContents.on("will-navigate", (event, url) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: "deny" };
+  });
 }
 
 app.whenReady().then(() => {
