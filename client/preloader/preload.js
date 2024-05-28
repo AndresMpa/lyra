9e0d37e2e2d72aa5d84666ea807621a875da84f6
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-27 21:12:37 -0500

    Loading Lyra's deploy URL

--- client/preloader/preload.js
+++ client/preloader/preload.js
@@ -1,4 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {
+const axios = require("axios");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const response = await axios.get(lyraUrl);
+  window.lyraBaseUrl = response.data;
+
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
