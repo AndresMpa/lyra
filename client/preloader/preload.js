0c49a51e76adc2fe46e940b33d700f3c3021c997
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-28 13:02:49 -0500

    Dynamic base url
    Tracking env vars on preloader to achieve pre fetch of Lyra
    dependencies, using that process to bring base urls

--- client/preloader/preload.js
+++ client/preloader/preload.js
@@ -1,9 +1,7 @@
 const axios = require("axios");
+require("dotenv").config();
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const response = await axios.get(lyraUrl);
-  window.lyraBaseUrl = response.data;
-
+const attachContextData = () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
@@ -12,4 +10,14 @@ window.addEventListener("DOMContentLoaded", async () => {
   for (const type of ["chrome", "node", "electron"]) {
     replaceText(`${type}-version`, process.versions[type]);
   }
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const baseUrl = process.env.LYRA_URL;
+
+  const lyraUrl = baseUrl;
+  const response = await axios.get(lyraUrl);
+  window.lyraBaseUrl = response.data;
+
+  attachContextData();
 });
