b3adc76400dc2967e45c3be1a5dd66f7cbd9d8a0
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-28 00:04:43 -0500

    New events & double enter
    Tracking new events and removing double enter

--- client/eventHandlers/main.js
+++ client/eventHandlers/main.js
@@ -1,11 +1,32 @@
+import {
+  addChatHandler,
+  prevChatHandler,
+  nextChatHandler,
+  menuHandler,
+} from "./navbar.js";
+import { loadDefaults } from "./window.js";
 import { handlePrompt } from "./prompt.js";
 
 const promptButton = document.querySelector("#promptButton");
 const promptChat = document.querySelector("#promptChat");
+const menuButton = document.querySelector("#menuButton");
+const prevChat = document.querySelector("#prevChat");
+const nextChat = document.querySelector("#nextChat");
+const addChat = document.querySelector("#addChat");
 
-promptButton.addEventListener("click", handlePrompt);
-promptChat.addEventListener("keydown", (event) => {
+window.addEventListener("DOMContentLoaded", loadDefaults);
+
+addChat.addEventListener("click", addChatHandler);
+prevChat.addEventListener("click", prevChatHandler);
+nextChat.addEventListener("click", nextChatHandler);
+menuButton.addEventListener("click", menuHandler);
+
+promptButton.addEventListener("click", async () => {
+  await handlePrompt();
+});
+promptChat.addEventListener("keydown", async (event) => {
   if (event.key === "Enter" && event.shiftKey) {
-    handlePrompt();
+    event.preventDefault();
+    await handlePrompt();
   }
 });
