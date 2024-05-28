e792e89dbac742988fa1e56e10921547f146ec3a
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-28 00:05:27 -0500

    Lyra's API
    Adding support to fetch API

--- client/eventHandlers/prompt.js
+++ client/eventHandlers/prompt.js
@@ -1,10 +1,33 @@
-const promptButton = document.querySelector("#promptButton");
+import {
+  saveItemToStorage,
+  saveArrayToStorage,
+  getItemFromStorage,
+} from "../utils/storageHandler.js";
+
 const promptChat = document.querySelector("#promptChat");
+
+const messageBox = document.querySelector("#messageBox");
+const activeChat = document.querySelector("#activeChat");
+const app = document.querySelector("#app");
+
+const trimBr = (htmlContent) => {
+  const tmpDiv = document.createElement("div");
+  tmpDiv.innerHTML = htmlContent;
+
+  while (tmpDiv.firstChild && tmpDiv.firstChild.nodeName === "BR") {
+    tmpDiv.removeChild(tmpDiv.firstChild);
+  }
+
+  while (tmpDiv.lastChild && tmpDiv.lastChild.nodeName === "BR") {
+    tmpDiv.removeChild(tmpDiv.lastChild);
+  }
+
+  return tmpDiv.innerHTML;
+};
 
 const createMessage = (content, messageType) => {
   const messageContainer = document.createElement("article");
   const message = document.createElement("p");
-
   message.classList.add(`${messageType}-message-content`);
   message.innerHTML = content;
 
@@ -14,21 +37,42 @@ const createMessage = (content, messageType) => {
   return messageContainer;
 };
 
-export const handlePrompt = () => {
-  const messageBox = document.querySelector("#messageBox");
-  const activeChat = document.querySelector("#activeChat");
-  const app = document.querySelector("#app");
+const askLyra = async (entry) => {
+  console.log(entry);
 
+  const rawData = await fetch(`${window.lyraBaseUrl}/chat`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user_input: entry }),
+  })
+    .then((response) => response.json())
+    .then((data) => data.response)
+    .catch((error) => console.error("Error en la solicitud fetch:", error));
+
+  return rawData;
+};
+
+export const handlePrompt = async () => {
   app.classList.add("moveUpFadeOut");
   app.classList.add("none");
 
   activeChat.classList = "active-chat";
   messageBox.classList.remove("none");
 
-  messageBox.append(createMessage(promptChat.innerHTML, "user"));
-  messageBox.scrollTop = messageBox.scrollHeight;
+  const currentChat = getItemFromStorage("currentChat");
+  const chats = getItemFromStorage("chats");
 
-  console.log(promptChat.innerHTML);
+  const rawContent = trimBr(promptChat.innerHTML);
+  saveArrayToStorage("chats", currentChat, { type: "user", data: rawContent });
+
+  messageBox.append(createMessage(rawContent, "user"));
+  messageBox.scrollTop = messageBox.scrollHeight;
   promptChat.replaceChildren();
-  console.log(promptChat.innerHTML);
+
+  const lyrasAnswer = await askLyra(rawContent);
+  saveArrayToStorage("chats", currentChat, { type: "lyra", data: lyrasAnswer });
+
+  messageBox.append(createMessage(lyrasAnswer, "lyra"));
+  messageBox.scrollTop = messageBox.scrollHeight;
+  promptChat.replaceChildren();
 };
