4cd8c74fe68a721c37655b94a07480bb46622888
Andres M. Prieto <andresprieto2703@gmail.com>
2024-05-30 06:11:20 -0500

    Widgets events
    Adding widgets events to load plots & re-render them

--- client/eventHandlers/main.js
+++ client/eventHandlers/main.js
@@ -7,6 +7,12 @@ import {
 } from "./navbar.js";
 import { loadDefaults } from "./window.js";
 import { handlePrompt } from "./prompt.js";
+import {
+  reloadCharts,
+  updateCpuChart,
+  updateRamChart,
+  updateDiskChart,
+} from "./widget.js";
 
 const promptButton = document.querySelector("#promptButton");
 const deleteChat = document.querySelector("#deleteChat");
@@ -16,7 +22,14 @@ const prevChat = document.querySelector("#prevChat");
 const nextChat = document.querySelector("#nextChat");
 const addChat = document.querySelector("#addChat");
 
-window.addEventListener("DOMContentLoaded", loadDefaults);
+window.addEventListener("DOMContentLoaded", () => {
+  loadDefaults();
+  updateCpuChart();
+  updateRamChart();
+  updateDiskChart();
+});
+
+window.addEventListener("resize", reloadCharts);
 
 menuButton.addEventListener("click", menuHandler);
 addChat.addEventListener("click", addChatHandler);
@@ -33,3 +46,7 @@ promptChat.addEventListener("keydown", async (event) => {
     await handlePrompt();
   }
 });
+
+setInterval(updateCpuChart, 1000); // Update the CPU chart every 1 second
+setInterval(updateRamChart, 1000); // Update the RAM chart every 1 second
+setInterval(updateDiskChart, 1000); // Update the Disk chart every 10 seconds
